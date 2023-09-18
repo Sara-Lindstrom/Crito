c1f54521ce0a59ca46b787edb868a4968f73fb9c
sara-lindstrom <sara.maria.lindstrom@hotmail.com>
2023-09-18 09:52:16 +0200

    three first weeks of work

---
+++ wwwroot/js/script.js
@@ -0,0 +1,14 @@
+try {
+    const toggleBtn = document.querySelector('[data-option="toggle"]')
+    toggleBtn.addEventListener('click', function () {
+        const element = document.querySelector(toggleBtn.getAttribute('data-target'))
+
+        if (!element.classList.contains('open-menu')) {
+            element.classList.add('open-menu')
+        }
+
+        else {
+            element.classList.remove('open-menu')
+        }
+    })
+} catch { }
